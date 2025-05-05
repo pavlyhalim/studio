fb6d2acea536ccbdae184404252735ae75c16fd8
Pavly Osama <Pavlyosama1000@gmail.com>
2025-05-05 15:19:21 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Runtime Error: Error: ./node_modules/@mapbox/node-pre-gyp/lib/util/nw-pre-gyp/index.html
    > Unknown module type
    > This module doesn't have an associated type. Use a known file extension, or register a loader for it.
    >
    > Read more: https://nextjs.org/docs/app/api-reference/next-config-js/turbo#webpack-loaders
    >
    > . Error source: Call Stack
    > 5
    >
    > Show 5 ignore-listed frame(s)

--- next.config.ts
+++ next.config.ts
@@ -26,21 +26,28 @@ const nextConfig: NextConfig = {
   },
   // Add Webpack config to ignore the problematic file/packages
   webpack: (config, { isServer, webpack }) => {
-    // Ignore problematic native module dependencies more broadly
+    // Ignore problematic native module dependencies globally
     config.plugins.push(
       new webpack.IgnorePlugin({
-        resourceRegExp: /^(node-pre-gyp|@mapbox\/node-pre-gyp|nw-pre-gyp)$/, // Ignore related packages
-        contextRegExp: /node_modules/, // Target only within node_modules
+        resourceRegExp: /^(node-pre-gyp|@mapbox\/node-pre-gyp|nw-pre-gyp)$/,
+        // Removed contextRegExp to apply ignore more broadly
       })
     );
 
-    // Specifically ignore the problematic HTML file with a slightly broader context
+    // Specifically ignore the problematic HTML file within the correct directory
     config.plugins.push(
       new webpack.IgnorePlugin({
-        resourceRegExp: /index\.html$/, // Target the specific file
-        contextRegExp: /node_modules\/@mapbox\/node-pre-gyp\/lib\/util$/, // Broaden context slightly
+        resourceRegExp: /index\.html$/,
+        contextRegExp: /node_modules\/@mapbox\/node-pre-gyp\/lib\/util$/, // Keep this specific context
       })
     );
+
+    // Add rule to handle .node files if bcrypt is causing issues
+    // (Less likely the cause of *this* error, but good practice for bcrypt)
+    config.module.rules.push({
+      test: /\.node$/,
+      use: 'node-loader', // Using 'use' for consistency
+    });
 
 
     // Important: return the modified config
